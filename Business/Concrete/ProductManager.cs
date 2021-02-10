972b2f7c047f193584e33160d08f607a490d02fd
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2021-02-10 18:00:13 +0300

    8. Gün bitti

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -9,18 +9,28 @@ namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        IProductDal productDal;
+        IProductDal _productDal;
 
         public ProductManager(IProductDal productDal)
         {
-            this.productDal = productDal;
+            this._productDal = productDal;
         }
 
         public List<Product> GetAll()
         {
             // İş Kodları
             // Yetkisi Var Mı?
-            return productDal.GetAll();
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategory(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public int CustomerId { get; set; }
+
+        public string ContactName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string City { get; set; }
+    }
+}
