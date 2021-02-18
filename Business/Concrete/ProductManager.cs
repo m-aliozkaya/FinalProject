e79f1da6e1d69308c17af34f5fc540478373fc3c
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2021-02-18 15:32:52 +0300

    12. Gün Kodları Eklendi. Aspect Oriented Programming için giriş yapıldı ve ValidationAspect yazıldı.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,12 +23,10 @@ namespace Business.Concrete
             this._productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //Business codes
 
             _productDal.Add(product);
 
